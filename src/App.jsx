7885461ffb865a0bf9cbf23a1dbbee87a66d55aa
bhaskarspak <bhaskar@spakengineering.com>
2023-07-03 08:05:11 +0530

    added lazy loading and loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,59 @@
-import { useState, useEffect, lazy } from 'react';
+import { lazy, Suspense } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import useDailyForecast from './hooks/useDailyForecast';
 import useGeolocation from './hooks/useGeolocation';
-import Home from './pages/Home';
-import History from './pages/History';
-import Maps from './pages/Maps';
-import Leaflet from './pages/Leaflet';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+
+// Lazy load the pages
+const Home = lazy(() => import('./pages/Home'));
+const History = lazy(() => import('./pages/History'));
+const Maps = lazy(() => import('./pages/Maps'));
+const Leaflet = lazy(() => import('./pages/Leaflet'));
 function App() {
   const { geoLocationData } = useGeolocation();
   // console.log(geoLocationData);
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/history" element={<History />} />
-        <Route
-          path="/precipitation"
-          element={
-            geoLocationData && (
-              <Leaflet
-                lat={geoLocationData.lat}
-                lon={geoLocationData.lon}
-                overlayType="precipitation_new"
-              />
-            )
-          }
-        />
-        <Route
-          path="/temperature"
-          element={
-            geoLocationData && (
-              <Leaflet
-                lat={geoLocationData.lat}
-                lon={geoLocationData.lon}
-                overlayType="temp_new"
-              />
-            )
-          }
-        />
-        <Route
-          path="/googlemaps"
-          element={
-            geoLocationData && (
-              <Maps lat={geoLocationData.lat} lon={geoLocationData.lon} />
-            )
-          }
-        />
-      </Routes>
-      {/* <Home /> */}
-      {/* <History /> */}
-      {/* {geoLocationData && (
-        <Maps
-        lat={geoLocationData.lat}
-        lon={geoLocationData.lon}
-        // overlayType="precipitation_new"
-        />
-      )} */}
-      {/* {geoLocationData && (
-        <Leaflet
-        lat={geoLocationData.lat}
-        lon={geoLocationData.lon}
-        overlayType="temp_new"
-        />
-      )} */}
+      <Suspense fallback={<div className="loading">Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/history" element={<History />} />
+          <Route
+            path="/precipitation"
+            element={
+              geoLocationData && (
+                <Leaflet
+                  lat={geoLocationData.lat}
+                  lon={geoLocationData.lon}
+                  overlayType="precipitation_new"
+                />
+              )
+            }
+          />
+          <Route
+            path="/temperature"
+            element={
+              geoLocationData && (
+                <Leaflet
+                  lat={geoLocationData.lat}
+                  lon={geoLocationData.lon}
+                  overlayType="temp_new"
+                />
+              )
+            }
+          />
+          <Route
+            path="/googlemaps"
+            element={
+              geoLocationData && (
+                <Maps lat={geoLocationData.lat} lon={geoLocationData.lon} />
+              )
+            }
+          />
+        </Routes>
+      </Suspense>
     </>
   );
 }
